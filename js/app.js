2456e612e7070e000d5a12026a368d30e1a75a78
Sal Ferrarello <sal@ironcodestudio.com>
2018-01-30 21:22:59 -0500

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,53 @@
+(function() {
+	var pause = false;
+
+	function getUnixTime( dateObj ) {
+		return Math.round(dateObj.getTime() / 1000);
+	}
+
+	function updateCurrentTimeValues() {
+		var dateObj = new Date();
+		if ( ! pause ) {
+			document.getElementById('current-unix-time').value  = getUnixTime(dateObj);
+			document.getElementById('current-utc-time').value   = dateObj.toUTCString();
+			document.getElementById('current-local-time').value = dateObj.toLocaleString();
+		}
+		setTimeout( updateCurrentTimeValues, 500 );
+	}
+
+	// Start updating current time values.
+	updateCurrentTimeValues();
+	// Pause update when Unix Time element is in focus.
+	var currentUnixTime = document.getElementById('current-unix-time');
+	currentUnixTime.addEventListener(
+		'focus', function(e) { pause = true;  }, true
+	);
+	currentUnixTime.addEventListener(
+		'blur',  function(e) { pause = false; }, true
+	);
+
+	// User entered Unix Time stamp.
+	document.getElementById('submit-user-unix-time').addEventListener(
+		'click',
+		function(e) {
+			var dateObj = new Date(document.getElementById('user-unix-time').value * 1000);
+			document.getElementById('user-utc-time').value   = dateObj.toUTCString();
+			document.getElementById('user-local-time').value = dateObj.toLocaleString();
+			e.preventDefault();
+			e.stopPropagation();
+		},
+		false
+	);
+
+	// User entered time string.
+	document.getElementById('submit-string-time').addEventListener(
+		'click',
+		function(e) {
+			var dateObj = new Date(document.getElementById('string-time').value);
+			document.getElementById('string-unix-time').value = getUnixTime(dateObj);
+			e.preventDefault();
+			e.stopPropagation();
+		},
+		false
+	);
+})();
