b6112922f7703edda7177a9d00b2a6ac572285d4
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2023-11-17 11:19:54 -0500

    Add convert to seconds function

--- js/app.js
+++ js/app.js
@@ -5,6 +5,29 @@
 		return Math.round(dateObj.getTime() / 1000);
 	}
 
+	/**
+	 * If the timestamp is in milliseconds, convert to seconds.
+	 * (It the timestamp is already in seconds, return it unmodified).
+	 */
+	function convertToSeconds( timestamp ) {
+		var currentTimeInMs = new Date().getTime();
+		if ( timestamp * 100 > currentTimeInMs ) {
+			// Assuming the timestamp provided were in seconds, multiplying
+			// it by 1000 should put it in the same neighborhood as the
+			// currentTimeInMS. Instead we are multiplying by only 100,
+			// so that result should still be less than the currentTimeInMS
+			// by approximately a factor of time.
+			//
+			// If the timestamp * 100 is greater than currentTimeInMS,
+			// we are assuming timestamp is in fact in ms (not seconds),
+			// so we divide it by 1000 to get seconds.
+			return timestamp / 1000;
+		}
+		// It looks like the given timestamp is already in seconds,
+		// return it unchanged.
+		return timestamp;
+	}
+
 	function updateCurrentTimeValues() {
 		var dateObj = new Date();
 		if ( ! pause ) {
@@ -31,7 +54,7 @@
 		'click',
 		function(e) {
 			var userInputTimestamp = document.getElementById('user-unix-time').value;
-			var dateObj = new Date( userInputTimestamp * 1000);
+			var dateObj = new Date( convertToSeconds( userInputTimestamp ) * 1000);
 			document.getElementById('user-utc-time').value   = dateObj.toUTCString();
 			document.getElementById('user-local-time').value = dateObj.toLocaleString();
 			e.preventDefault();
